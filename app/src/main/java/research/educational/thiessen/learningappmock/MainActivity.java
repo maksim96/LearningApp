ee7b716f3e9394a0a8395d29f1923cb680c53c30
Maximilian Thiessen <MaxmilianThiessen96@gmail.com>
2018-05-20 17:29:41 +0200

    Simple test with onClickListeners.

---
+++ app/src/main/java/research/educational/thiessen/learningappmock/MainActivity.java
@@ -0,0 +1,85 @@
+package research.educational.thiessen.learningappmock;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private boolean firstTime = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //specify the button that has to handle visibility
+        ImageView forest = (ImageView) findViewById(R.id.imageForest);
+
+//And the layout we want to change is visibility
+        final ImageView unicorn = (ImageView) findViewById(R.id.imageUnicorn);
+        final ImageView bubble = (ImageView) findViewById(R.id.bubble);
+        final TextView bubbleText = (TextView) findViewById(R.id.bubbleText);
+
+        forest.setOnClickListener
+                (
+                        new View.OnClickListener()
+                        {
+                            public void onClick(View v)
+                            {
+                                if (unicorn.getVisibility() == View.VISIBLE)
+                                {
+                                    unicorn.setVisibility(View.INVISIBLE);
+                                    unicorn.setClickable(false);
+                                    bubble.setVisibility(View.INVISIBLE);
+                                    bubbleText.setVisibility(View.INVISIBLE);
+                                }
+                                else
+                                {
+                                    unicorn.setVisibility(View.VISIBLE);
+                                    unicorn.setClickable(true);
+                                }
+                            }
+                        }
+                );
+
+
+//And the layout we want to change is visibility
+
+
+        unicorn.setOnClickListener
+                (
+                        new View.OnClickListener()
+                        {
+                            public void onClick(View v)
+                            {
+                                if (!firstTime) {
+                                    int x = (int) (1+Math.random()*10);
+                                    int y = (int) (1+Math.random()*10);
+                                    bubbleText.setText(""+x+" x " +y + "?");
+
+                                }
+                                firstTime = false;
+
+                                if (bubble.getVisibility() == View.VISIBLE)
+                                {
+
+                                }
+                                else
+                                {
+                                    bubble.setVisibility(View.VISIBLE);
+                                    bubbleText.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        }
+                );
+    }
+}
