a96cadba7606f94a110b7f6be14ac100ea1f1b72
Maximilian Thiessen <MaxmilianThiessen96@gmail.com>
2018-05-26 18:08:53 +0200

    Drag and drop is working.

--- app/src/main/java/research/educational/thiessen/learningappmock/DragActivity.java
+++ app/src/main/java/research/educational/thiessen/learningappmock/DragActivity.java
@@ -9,6 +9,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -16,6 +19,8 @@ import android.support.v4.app.NavUtils;
  */
 public class DragActivity extends Activity {
 
+    private ImageView nuts[];
+    private ViewGroup rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,60 @@ public class DragActivity extends Activity {
 
         setContentView(R.layout.activity_drag);
 
+        rootLayout = (ViewGroup) findViewById(R.id.drag_root);
+
+        nuts = new ImageView[4];
+
+        int[] nutIds = new int[] { R.id.nuts, R.id.nuts2, R.id.nuts3, R.id.nuts4 };
+
+        for (int i = 0; i < 4; i++) {
+            nuts[i] = findViewById(nutIds[i]);
+            nuts[i].setOnTouchListener(new ChoiceTouchListener());
+        }
+
+
+
+
+     /*   RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(getPx(nut.getWidth()), getPx(nut.getHeight()));
+        layoutParams.leftMargin = getPx(nut.getX());
+        layoutParams.topMargin = getPx(nut.getY());
+
+        nut.setLayoutParams(layoutParams);*/
+
+
+    }
+
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            final int x = (int) motionEvent.getRawX();
+            final int y = (int) motionEvent.getRawY();
+            int xDelta = 0, yDelta = 0;
+            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+            switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+
+                    xDelta = x - lParams.leftMargin;
+                    yDelta = y - lParams.topMargin;
+                    break;
+             /*   case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;*/
+                case MotionEvent.ACTION_MOVE:
+                    lParams.leftMargin = x - xDelta;
+                    lParams.topMargin = y - yDelta;
+                    lParams.rightMargin = -250;
+                    lParams.bottomMargin = -250;
+                    view.setLayoutParams(lParams);
+                    break;
+            }
+            rootLayout.invalidate();
+            return true;
+        }
     }
 
     @Override
@@ -32,6 +91,11 @@ public class DragActivity extends Activity {
         // Trigger the initial hide() shortly after the activity has been
         // created, to briefly hint to the user that UI controls
         // are available.
+    }
+
+    private int getPx(float dp) {
+        float density = rootLayout.getContext().getResources().getDisplayMetrics().density;
+        return (int)((dp * density) + 0.5);
     }
 
 
